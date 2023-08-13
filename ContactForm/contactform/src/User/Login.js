7980c15243f009a94ec76b5b73575a0917df149b
MachchanandhiniS <machchanandhinisathiswaran@gmail.com>
2023-08-13 09:38:47 +0000

    ContactForm

---
+++ ContactForm/contactform/src/User/Login.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ToastContainer,toast } from 'react-toastify';
+
+const defaultTheme = createTheme();
+
+export default function SignIn(props) {
+    const [errorEmail, setErrorEmail] = useState(false);
+    const [errorPassword, setErrorPassword] = useState(false);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.get('email'))) {
+            setErrorEmail(false)
+        } else {
+            setErrorEmail(true)
+        }
+        if (/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(data.get('mobileNumber'))) {
+            setErrorPassword(false)
+        } else {
+            setErrorPassword(true)
+        }
+        if(!errorEmail && data.get('email') === 'abc@gmail.com' && data.get('password') === 'abc'){
+            toast('Login Successfull')
+            setTimeout(()=>{
+                props.setLoginpage(false);
+            },[5000])
+        }else{
+            toast('Enter correct email')
+        }
+
+    };
+
+    return (
+        <ThemeProvider theme={defaultTheme} >
+            <Container component="main" maxWidth="xs">
+                {/* <CssBaseline /> */}
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        position: 'relative'
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                        <TextField
+                            error={errorEmail}
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            helperText={errorEmail ? 'Incorrect email' : ''}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <ToastContainer/>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+}
